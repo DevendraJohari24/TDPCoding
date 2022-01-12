d0c7aacb867b20b36a5301a5044de5b1f5a3d271
Devendra Johari <54894480+DevendraJohari24@users.noreply.github.com>
2022-01-12 10:05:12 +0530

    commit

--- Pattern Questions/Simple/Pattern8.cpp
+++ Pattern Questions/Simple/Pattern8.cpp
@@ -21,9 +21,26 @@ int main(){
         for(int j=n-i;j>=1;j--){
             cout<<" ";
         }
+        for(int j=2*i-1;j>=1;j--){
+            if(j==1 || j==(2*i-1)){
+                cout<<i;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+    } 
+ // Downwards
+    for(int i=1;i<n;i++){
         for(int j=1;j<=i;j++){
-            cout<<i;
-            for(int k=1;k<i;k++){
+            cout<<" ";
+        }
+        for(int j=(2*(n-i)-1);j>=1;j--){
+            if(j==1 || j==(2*(n-i)-1)){
+                cout<<n-i;
+            }
+            else{
                 cout<<" ";
             }
         }
---
+++ Pattern Questions/Simple/Pattern9.cpp
@@ -0,0 +1,35 @@
+/*
+E       E 
+ D     D
+  C   C
+   B B
+    A  
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int n;
+    char ch;
+    cin>>n;
+    int ascii = 65+n-1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=2*(n-i)+1;j>=1;j--){
+            if(j==1 || j==(2*(n-i)+1)){
+                ch = ascii;
+                cout<<ch;
+            }
+            else{
+                cout<<" ";
+            }   
+        }
+        ascii--;
+        cout<<"\n";
+    }
+    return 0;
+
+}
